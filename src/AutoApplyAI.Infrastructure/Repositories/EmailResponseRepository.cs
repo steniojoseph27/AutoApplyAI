33487b044b2f07ba5dd89191c1e6dbf01824d2e5
steniojoseph27 <steniojoseph@hotmail.com>
2024-09-17 19:57:11 -0400

    Feat: Persistence and Repositories - Infrastructure Layer - Add JobApplicationRepository.cs and EmailResponseRepository.cs - Add DbContext: ApplicationDbContext.cs in Infrastructure Layer

---
+++ src/AutoApplyAI.Infrastructure/Repositories/EmailResponseRepository.cs
@@ -0,0 +1,16 @@
+using AutoApplyAI.Domain.Entities; 
+using AutoApplyAI.Domain.Interfaces; 
+namespace AutoApplyAI.Infrastructure.Repositories 
+{ 
+    public class EmailResponseRepository : IEmailResponseRepository 
+    { 
+        public void Add(EmailResponse emailResponse) 
+        { 
+
+        } 
+        public EmailResponse GetById(int id) 
+        { 
+            return null; 
+        } 
+    } 
+}

7f3fb4857de9c2c263f6100c12d79bcd7c9fac79
steniojoseph27 <steniojoseph@hotmail.com>
2024-09-19 04:29:34 -0400

    Refactor: - Complete JobApplicationController implementation.

--- src/AutoApplyAI.WebAPI/Controllers/JobApplicationController.cs
+++ src/AutoApplyAI.WebAPI/Controllers/JobApplicationController.cs
@@ -1,19 +1,27 @@
+using AutoApplyAI.Application.DTOs;
 using AutoApplyAI.Application.Interfaces; 
 using Microsoft.AspNetCore.Mvc; 
 namespace AutoApplyAI.WebAPI.Controllers 
 { 
     [Route("api/[controller]")] [ApiController] 
     public class JobApplicationController : ControllerBase 
-    { 
-        private readonly IJobApplicationService _service; 
-        public JobApplicationController(IJobApplicationService service) 
-        { 
-            _service = service; 
-        } 
-        [HttpPost] 
-        public IActionResult Apply() 
-        { 
-            return Ok(); 
-        } 
+    {
+        private readonly IJobApplicationService _jobApplicationService;
+
+        public JobApplicationController(IJobApplicationService jobApplicationService)
+        {
+            _jobApplicationService = jobApplicationService;
+        }
+
+        [HttpPost]
+        public IActionResult ApplyToJob([FromBody] JobOfferDto jobOfferDto, string candidateSkills)
+        {
+            var applied = _jobApplicationService.ApplyToJob(jobOfferDto, candidateSkills);
+            if (applied)
+            {
+                return Ok("Job application submitted.");
+            }
+            return BadRequest("Job not relevant to your skills.");
+        }
     } 
 }

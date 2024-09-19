779dc6efab788f1bc415e9da3fb59d2744a67342
steniojoseph27 <steniojoseph@hotmail.com>
2024-09-19 03:44:59 -0400

    Feat: - Complete the implementation of the interfaces: - IJobApplicationService.cs add the ApplyToJob method. - IEmailResponderService add the SendEmailResponseAsync method.

--- src/AutoApplyAI.Application/Interfaces/IEmailResponderService.cs
+++ src/AutoApplyAI.Application/Interfaces/IEmailResponderService.cs
@@ -1,7 +1,7 @@
 namespace AutoApplyAI.Application.Interfaces 
 { 
     public interface IEmailResponderService 
-    { 
-
+    {
+        Task SendEmailResponseAsync(string recipientEmail, string subject, string messageBody);
     } 
 }
--- src/AutoApplyAI.Application/Interfaces/IJobApplicationService.cs
+++ src/AutoApplyAI.Application/Interfaces/IJobApplicationService.cs
@@ -1,7 +1,9 @@
+using AutoApplyAI.Application.DTOs;
+
 namespace AutoApplyAI.Application.Interfaces 
 { 
     public interface IJobApplicationService 
     {
-
+        public bool ApplyToJob(JobOfferDto jobOfferDto, string candidateSkills);
     } 
 }

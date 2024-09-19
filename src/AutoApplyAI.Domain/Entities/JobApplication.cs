911649496db16ca704edbddbeeb3596bcb72f392
steniojoseph27 <steniojoseph@hotmail.com>
2024-09-19 04:24:38 -0400

    Refactor: - Modify JobApplication entity initialize the properties to empty string where that applies - Complete the EmailResponderController class implementation. - Add the EmailRequest.cs model.

--- src/AutoApplyAI.Domain/Entities/JobApplication.cs
+++ src/AutoApplyAI.Domain/Entities/JobApplication.cs
@@ -3,9 +3,9 @@ namespace AutoApplyAI.Domain.Entities
     public class JobApplication 
     { 
         public int Id { get; set; } 
-        public string JobTitle { get; set; } 
-        public string CompanyName { get; set; } 
-        public string Skills { get; set; } 
-        public DateTime DateApplied { get; set; } 
+        public string JobTitle { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        public string Skills { get; set; } = string.Empty;
+        public DateTime DateApplied { get; set; }
     } 
 }
---
+++ src/AutoApplyAI.WebAPI/Request/EmailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoApplyAI.WebAPI.Request
+{
+    public class EmailRequest
+    {
+        public string RecipientEmail { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string MessageBody { get; set; } = string.Empty;
+    }
+}

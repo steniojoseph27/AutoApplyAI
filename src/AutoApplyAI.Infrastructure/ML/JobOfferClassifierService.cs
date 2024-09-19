df87edaf2aba47107d7d8c9072bf20b3c22f4aaf
steniojoseph27 <steniojoseph@hotmail.com>
2024-09-19 04:35:53 -0400

    Feat: - Implement the logic to load the ML.NET model and make predictions: JobOfferInput.cs, JobOfferPrediction.cs. - Implement the ML.NET JobOfferClassifierService.cs

---
+++ src/AutoApplyAI.Infrastructure/ML/JobOfferClassifierService.cs
@@ -0,0 +1,47 @@
+﻿using AutoApplyAI.Domain.Entities;
+using AutoApplyAI.Domain.Interfaces;
+using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoApplyAI.Infrastructure.ML
+{
+    public class JobOfferClassifierService : IJobOfferClassifierService
+    {
+        private readonly MLContext _mlContext;
+        private readonly ITransformer _model;
+
+        public JobOfferClassifierService()
+        {
+            _mlContext = new MLContext();
+            _model = LoadModel();
+        }
+
+        private ITransformer LoadModel()
+        {
+            // Load the pre-trained ML.NET model
+            string modelPath = "path_to_model/JobOfferClassifier.zip";
+            return _mlContext.Model.Load(modelPath, out _);
+        }
+
+        public bool IsRelevantJob(JobOffer jobOffer, string candidateSkills)
+        {
+            // Create input data for prediction
+            var input = new JobOfferInput
+            {
+                JobTitle = jobOffer.JobTitle,
+                JobDescription = jobOffer.JobDescription,
+                RequiredSkills = jobOffer.RequiredSkills
+            };
+
+            // Predict relevance using the ML.NET model
+            var predictionEngine = _mlContext.Model.CreatePredictionEngine<JobOfferInput, JobOfferPrediction>(_model);
+            var prediction = predictionEngine.Predict(input);
+
+            return prediction.IsRelevant;
+        }
+    }
+}
---
+++ src/AutoApplyAI.Infrastructure/ML/JobOfferPrediction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoApplyAI.Infrastructure.ML
+{
+    public class JobOfferPrediction
+    {
+        public bool IsRelevant { get; set; }
+    }
+}
